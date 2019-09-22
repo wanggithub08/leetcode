75e52f14f7deec581d51ec9dfa11f0d26c4edd00
wangtongli <wangli3508@gmail.com>
2019-09-22 15:36:26 +0800

    commit fileheader

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
+/*
+ * @Description: "the entry point of our executable program."
+ * @Date: 2019-09-22 15:05:54
+ * @LastEditors: wangtongli
+ * @Email: wangli3508@gmail.com
+ * @LastEditTime: 2019-09-22 15:35:25
+ */
+
 package main
 
 import (
 	"fmt"
 )
 
-func main(){
+func main() {
 	fmt.Println("hello wrold!")
 }
