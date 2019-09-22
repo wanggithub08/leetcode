a55b4edfa0d78a6e97f088de60daff50af94947d
wangtongli <wangli3508@gmail.com>
2019-09-22 16:23:07 +0800

    commit sqrt

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
+package main
+
 /*
  * @Description: "the entry point of our executable program."
  * @Date: 2019-09-22 15:05:54
  * @LastEditors: wangtongli
  * @Email: wangli3508@gmail.com
- * @LastEditTime: 2019-09-22 15:35:25
+ * @LastEditTime: 2019-09-22 16:17:14
  */
-
-package main
 
 import (
 	"fmt"
 )
 
 func main() {
-	fmt.Println("hello wrold!")
+	fmt.Println(mySqrt(9))
 }
---
+++ mysqrt.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+ * @Description: "sqrt function"
+ * @Date: 2019-09-22 16:08:09
+ * @LastEditors: wangtongli
+ * @Email: wangli3508@gmail.com
+ * @LastEditTime: 2019-09-22 16:21:02
+ */
+
+func mySqrt(x int) int {
+	return 0
+}
